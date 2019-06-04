78df87b6f1884f80259d2aec23d5e29b93d17e59
poushitaguha <pguha.kolkata@gmail.com>
2019-06-04 22:51:11 +0000

    Added code in express_server js file for URL shortening part 1

--- express_server.js
+++ express_server.js
@@ -2,7 +2,15 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
+
+// Function to generate random strings
+function generateRandomString() {
+
+}
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -12,6 +20,15 @@ var urlDatabase = {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
